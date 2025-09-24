82cf9449391734d0c5da50e5ad6cc1928ce2e46f
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-24 09:35:10 +0530

    create update profile logics for vendors

--- backend/controllers/vendor_controller.js
+++ backend/controllers/vendor_controller.js
@@ -87,6 +87,66 @@ const vendorController = {
       });
     }
   }),
+
+  updateVendorProfile: asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+
+    //find vendor current profile
+    const vendorProfile = await VendorApplication.findOne({ userId });
+    if (!vendorProfile) {
+      res.status(400).json({
+        message: "Vendor profile not found for this user",
+      });
+    }
+
+    //desctruture req.body
+    const {
+      businessName,
+      taxId,
+      category,
+      certifications,
+      businessDescription,
+      contactPerson,
+      businessAddress,
+      businessRegistrationNumber,
+      storeType,
+      paymentDetails,
+    } = req.body;
+
+    //handle images logics
+    const shopImages = req.files?.shopImages ? req.files.shopImages.map((file) => file.path) : vendorProfile.shopImages;
+
+    //handle product details with images
+     const productImages = req.files?.productImages
+      ? req.files.productImages.map((file, index) => ({
+          title: req.body[`productTitle_${index}`],
+          description: req.body[`productDescription_${index}`],
+          images: [file.path],
+      }))
+      : vendorProfile.initialProductList;
+
+    //update vendor profile
+    vendorProfile.businessName = businessName || vendorProfile.businessName;
+    vendorProfile.taxId = taxId || vendorProfile.taxId;
+    vendorProfile.category = category || vendorProfile.category;
+    vendorProfile.certifications = certifications || vendorProfile.certifications;
+    vendorProfile.businessDescription = businessDescription || vendorProfile.businessDescription;
+    vendorProfile.contactPerson = contactPerson || vendorProfile.contactPerson;
+    vendorProfile.businessAddress = businessAddress || vendorProfile.businessAddress;
+    vendorProfile.businessRegistrationNumber = businessRegistrationNumber || vendorProfile.businessRegistrationNumber;
+    vendorProfile.storeType = storeType || vendorProfile.storeType;
+    vendorProfile.paymentDetails = paymentDetails || vendorProfile.paymentDetails;
+    vendorProfile.shopImages = shopImages; // Updated shop images
+    vendorProfile.initialProductList = productImages; // Updated product images
+
+    //save new vendor profile
+    await vendorProfile.save();
+
+    res.status(200).json({
+      message: "Vendor profile updated successfully",
+      vendorProfile,
+    });
+  }),
 };
 
 module.exports = vendorController;
--- backend/routes/vendorRouter.js
+++ backend/routes/vendorRouter.js
@@ -2,11 +2,18 @@ const express = require("express");
 const vendorController = require("../controllers/vendor_controller");
 const isAuthenticatedUser = require("../middlewares/auth_user");
 const { upload } = require("../service/cloudinary");
+const hasRole = require("../middlewares/has_role_middleware");
 const vendorRouter = express.Router();
 
 vendorRouter.post("/vendor/apply",isAuthenticatedUser,upload.fields([
     {name:'shopImages',maxCount:5},
     {name:'productImages',maxCount:10}
 ]) ,vendorController.createVendorApplication);
+
+vendorRouter.put('/vendor/update',isAuthenticatedUser,hasRole(['vendor']),upload.fields([
+    {name:'shopImages',maxCount:5},
+    {name:'productImages',maxCount:10}
+]),vendorController.updateVendorApplication);
+
 
 module.exports = vendorRouter;
