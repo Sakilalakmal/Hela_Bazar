ba9d5d84f83e2b68d68785a3b07378a2ebe18b79
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-24 09:52:35 +0530

    initialize controller for products

---
+++ backend/controllers/product_controller.js
@@ -0,0 +1,8 @@
+const asyncHandler = require('express-async-handler');
+const Product = require('../model/product_model');
+
+const productController = {
+    createProducts: asyncHandler(async(req,res)=>{
+        
+    }),
+};
