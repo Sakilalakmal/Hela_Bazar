f2c5952c6a48a658a0271b910d48b825952c539e
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-24 14:34:50 +0530

    implement logic for delete cart item

--- backend/controllers/order_controller.js
+++ backend/controllers/order_controller.js
@@ -53,6 +53,38 @@ const OrderManagementController = {
       cart,
     });
   }),
-};
 
+  deleteFromCart: asyncHandler(async (req, res) => {
+    const userId = req.user.id;
+    const productId = req.params;
+
+    //check if the user's cart exists to delete
+    const cart = await Cart.findOne({ userId });
+    if (!cart) {
+      res.status(404).json({
+        message: "Cart not found to delete",
+      });
+    }
+
+    if(String(cart.userId) !== String(userId)){
+      res.status(403).json({
+        message: "You are not authorized to delete items from this cart",
+      });
+    }
+
+    // Remove product from cart (no customization logic needed)
+    cart.items = cart.items.filter(
+      (item) => String(item.productId) !== String(productId)
+    );
+
+    cart.updatedAt = Date.now();
+    await cart.save();
+
+    res.status(200).json({
+      message: "Product removed from cart successfully",
+      cart,
+    });
+
+  }),
+};
 module.exports = OrderManagementController;
