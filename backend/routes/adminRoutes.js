4b237a4797d256f06bbf4e487752c20a55beb91f
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-27 15:02:31 +0530

    admin routes declare

---
+++ backend/routes/adminRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const isAuthenticatedUser = require("../middlewares/auth_user");
+const hasRole = require("../middlewares/has_role_middleware");
+const adminController = require("../controllers/admin_controller");
+const adminRouter = express.Router();
+
+adminRouter.post(
+  "/approve/vendor/:applicationId",
+  isAuthenticatedUser,
+  hasRole(["admin"]),
+  adminController.vendorApproved
+);
+
+module.exports = adminRouter;
