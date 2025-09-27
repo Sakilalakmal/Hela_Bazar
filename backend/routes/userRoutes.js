cb58f43ce16998e92da6f3aa514165ee2b1c5720
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-27 12:53:16 +0530

    create user rouetr for request otp

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,5 +5,6 @@ const userRouter = express.Router();
 
 userRouter.post('/user/register',userController.registerUser);
 userRouter.post('/user/login',userController.loginUser);
+userRouter.post('/request/otp',userController.otpSend);
 
 module.exports = userRouter;
