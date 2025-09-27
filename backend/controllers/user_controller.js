69ee508a28b284c207e5af81ec84c9751d594c3d
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-27 11:52:34 +0530

    create  generate otp function

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -78,6 +78,14 @@ const userController = {
       res.status(500).json({ message: "otp send error", error: error.message });
     }
   }),
+
+
 };
+// Generate a random 6-digit OTP
+function generateOTP() {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+}
+
+
 
 module.exports = userController;
