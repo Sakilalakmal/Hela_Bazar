046cae92a16032cfaaa1219f2fddd161d23b23ca
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-27 12:52:50 +0530

    changed send email logics

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,36 +1,30 @@
-const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
-dotenv.config();
+const nodemailer = require("nodemailer");
 
-const sendEmail = async (to , message) => {
+const sendEmail = async (to, message, subject = "OTP for Hela Bazar Login") => {
   try {
-    //create transporter
     const transporter = nodemailer.createTransport({
-        host:"smtp.gmail.com",
-        port:587,
-        secure:false,
-        auth:{
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASS,
-        }
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      }
     });
 
-    //message
     const mailOptions = {
-        to,
-        subject:"New Message from Tourist Lanka.com",
-        html:`<p>${message}</p>`,
-    }
+      from: process.env.EMAIL_USER, // Always set a "from"
+      to,
+      subject,
+      html: `<p>${message}</p>`,
+    };
 
-    //send the email
-   const resInfo = await transporter.sendMail(mailOptions);
-   console.log("Message sent:",resInfo.messageId);
-   
+    const resInfo = await transporter.sendMail(mailOptions);
+    console.log("Message sent:", resInfo.messageId);
 
   } catch (error) {
     console.log(error);
-    throw new Error("Email not sent try again later");
-    
+    throw new Error("Email not sent, try again later");
   }
 };
 
