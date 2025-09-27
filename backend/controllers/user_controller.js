796d3aa8e8f079dc1770829e68f8c204b5f40051
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-27 13:35:06 +0530

    change login logic in logic controller

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -44,34 +44,47 @@ const userController = {
   }),
 
   loginUser: asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+    const { email, otp } = req.body;
 
-    if (!email || !password) {
+    if (!email || !otp) {
       res.status(400).json({
-        messsage: "please provide email and password",
+        messsage: "please provide email and otp",
       });
     }
 
-    //check user by email if user exists in db throw true
+    //check otp for this email
+    const otpRecord = await Otp.findOne({ email }).sort({ createdAt: -1 });
+    if (!otpRecord) {
+      return res.status(400).json({ message: "Otp not found..." });
+    }
+
+    if (otpRecord.otp !== otp || otpRecord.expiresAt < Date.now()) {
+      return res.status(400).json({ message: "Invalid Otp" });
+    }
+
     const userExists = await User.findOne({ email });
-    if (userExists && (await bcrypt.compare(password, userExists.password))) {
-      //generate web token for login user when user login in to sysytem
-      const token = await jwt.sign(
-        { id: userExists._id, role: userExists.role },
-        process.env.JWT_SECRET,
-        {
-          expiresIn: "30d",
-        }
-      );
-
-      res.status(201).json({
-        _id: userExists._id,
-        username: userExists.username,
-        email: userExists.email,
-        role: userExists.role,
-        token: token,
-      });
+    if (!userExists) {
+      return res.status(404).json({ message: "User not found" });
     }
+
+    await Otp.deleteMany({ email });
+
+    //generate web token for login user when user login in to sysytem
+    const token = await jwt.sign(
+      { id: userExists._id, role: userExists.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    res.status(201).json({
+      _id: userExists._id,
+      username: userExists.username,
+      email: userExists.email,
+      role: userExists.role,
+      token: token,
+    });
   }),
 
   otpSend: asyncHandler(async (req, res) => {
@@ -90,7 +103,7 @@ const userController = {
 
       //generate otp
       let otp = generateOTP();
-      let expiresAt = Date.now() + 30 * 1000; // OTP valid for 30 seconds
+      let expiresAt = new Date(Date.now() + 30 * 1000); // OTP valid for 30 seconds
 
       // Remove any old OTPs for this email
       await Otp.deleteMany({ email });
