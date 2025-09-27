9243905d464ee619d496899d107c94d21e37292e
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-27 14:01:13 +0530

    add changed and confirm routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const userController = require('../controllers/user_controller');
+const express = require("express");
+const userController = require("../controllers/user_controller");
 const userRouter = express.Router();
 
-
-userRouter.post('/user/register',userController.registerUser);
-userRouter.post('/user/login',userController.loginUser);
-userRouter.post('/request/otp',userController.otpSend);
+userRouter.post("/user/register", userController.registerUser);
+userRouter.post("/user/login", userController.loginUser);
+userRouter.post("/request/otp", userController.otpSend);
+userRouter.post("/change/email", userController.changeEmail);
+userRouter.post("/confirm/changed-email", userController.confirmChangeEmail);
 
 module.exports = userRouter;
