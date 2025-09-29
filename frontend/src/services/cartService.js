7bd6c43f72b16daaeca8526e082773d6956a5752
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-29 20:39:06 +0530

    add all service logics for carts

--- frontend/src/services/cartService.js
+++ frontend/src/services/cartService.js
@@ -31,3 +31,31 @@ export async function getCart(token) {
     throw new Error((await res.json()).message || "Failed to get cart");
   return res.json(); // { message, cart }
 }
+
+//! Remove item from cart
+// Remove one item from cart
+export async function removeFromCart(productId, token) {
+  const res = await fetch(`${API_URL}/orders/cart/remove/${productId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok)
+    throw new Error((await res.json()).message || "Failed to remove item");
+  return res.json();
+}
+
+//! remove cart item from list
+export async function clearCart(token) {
+  const res = await fetch(`${API_URL}/orders/cart/removeAll`, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) throw new Error((await res.json()).message || "Failed to clear cart");
+  return res.json();
+}
