5f2b9164fbb6fcd84351042fa59ebcd319a483b8
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-29 11:56:18 +0530

    change import for useAuth

--- frontend/src/pages/requestOtp.jsx
+++ frontend/src/pages/requestOtp.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { sendOtp, loginUser } from "../services/authService";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/authContext";
 
 function AuthPage() {
   const navigate = useNavigate();
