c57d42135cacae43331af451ab9f9ed848e0fc5c
sakilalakmal <sakilalakmal77@gmail.com>
2025-09-29 11:36:16 +0530

    get wishlist logic implementation

--- frontend/src/services/wishlistService.js
+++ frontend/src/services/wishlistService.js
@@ -5,10 +5,41 @@ export async function addToWishlist(productId, token) {
   const res = await fetch(`${API_URL}/wishlist/add/${productId}`, {
     method: "POST",
     headers: {
-      "Authorization": "Bearer " + token,
-      "Content-Type": "application/json"
-    }
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
   });
-  if (!res.ok) throw new Error((await res.json()).message || "Failed to add to wishlist");
+  if (!res.ok)
+    throw new Error((await res.json()).message || "Failed to add to wishlist");
+  return res.json();
+}
+
+//! Get wishlist items
+export async function getWishlist(token) {
+  const res = await fetch(`${API_URL}/wishlist/all/items`, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok)
+    throw new Error((await res.json()).message || "Failed to get wishlist");
+  return res.json();
+}
+
+//! Remove product from wishlist
+export async function removeFromWishlist(productId, token) {
+  const res = await fetch(`${API_URL}/wishlist/remove/${productId}`, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok)
+    throw new Error(
+      (await res.json()).message || "Failed to remove from wishlist"
+    );
   return res.json();
 }
